124c11d0baa4039df7750d0a855d3b1bd530729e
Rene Christen <accounts@renechristen.net>
2016-02-05 15:12:06 +1100

    first commit

---
+++ src/cocStencil.h
@@ -0,0 +1,49 @@
+/*
+
+code adapted from:  
+http://www.videotutorialsrock.com/opengl_tutorial/reflections/video.php
+
+*/
+
+#pragma once
+
+#ifdef COC_CI
+#include "cinder/gl/gl.h"
+#endif
+
+#ifdef COC_OF
+#include "ofMain.h"
+#endif
+
+namespace coc {
+	
+namespace stencil {
+
+    //set up to draw stencil
+    static void beginDrawingStencil() {
+        glClear(GL_STENCIL_BUFFER_BIT);
+        glEnable(GL_STENCIL_TEST); //Enable using the stencil buffer
+        glColorMask(0, 0, 0, 0); //Disable drawing colors to the screen
+        glStencilFunc(GL_ALWAYS, 1, 1); //Make the stencil test always pass
+        //Make pixels in the stencil buffer be set to 1 when the stencil test passes
+        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+        //Set all of the pixels below to be 1 in the stencil buffer...
+    }
+
+    //switch from drawing stencil to scene to be masked
+    static void beginUsingStencil(bool _invert = 0) {
+        glColorMask(1, 1, 1, 1); //Enable drawing colors to the screen
+        //Make the stencil test pass only when the pixel is 1 in the stencil buffer unless inverted
+        _invert ? glStencilFunc(GL_NOTEQUAL, 1, 1) : glStencilFunc(GL_EQUAL, 1, 1);
+        glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP); //Make the stencil buffer not change
+        //Draw all pixels where the stencil buffer is 1...
+    }
+
+    //back to normal unmasked drawing
+    static void endUsingStencil() {
+        glDisable(GL_STENCIL_TEST); //Disable using the stencil buffer
+    }
+
+}//namespace stencil
+
+}//namespace coc
